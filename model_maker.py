23e44e5c8836014718a5abc142be21cce8ef550d
NKabaso <kabasohouse@gmail.com>
2026-05-22 14:45:05 -0400

    Successfully trained model. Limited to 100 pictures per category

---
+++ model_maker.py
@@ -0,0 +1,81 @@
+import os
+import tensorflow as tf
+import cv2
+import mediapipe as mp
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import joblib
+
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=True)
+    
+def extract_hand_landmarks(image):
+    if image is None:
+        return None
+    
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # Convert the image to RGB format
+    results = hands.process(rgb_image) # Process the image to detect hand landmarks
+    
+    if not results.multi_hand_landmarks:
+        return None
+    
+    hand = results.multi_hand_landmarks[0] # Get the first detected hand
+    landmarks = []
+    for landmark in hand.landmark:
+        landmarks.append((landmark.x, landmark.y, landmark.z)) # Append the normalized coordinates of the landmark
+        
+    return landmarks
+
+def build_dataset(data_file_path):
+    if not os.path.exists(data_file_path):
+        print(f"Error: The directory '{data_file_path}' does not exist.")
+        return None, None
+    X =[] #data
+    Y = [] #labels
+    for label in os.listdir(data_file_path):
+        folder_path = os.path.join(data_file_path, label)
+        print(f"Processing folder: {folder_path} with label: {label}")
+        cnt = 0
+        if os.path.isdir(folder_path):
+            for image_file in os.listdir(folder_path):
+                if cnt >= 100: # Limit to 100 images per class
+                    break
+                image_path = os.path.join(folder_path, image_file)
+                image = cv2.imread(image_path)
+                if image is None:
+                    print("FAILED TO LOAD:", image_path)
+                    continue
+
+                landmarks = extract_hand_landmarks(image)
+                if landmarks is not None:
+                    X.append(landmarks)
+                    Y.append(label)
+                    cnt += 1
+        else:
+            print(f"Warning: '{folder_path}' is not a directory and will be skipped.")
+
+    return np.array(X), np.array(Y)
+
+def train(X, Y):
+    print("Dataset dimensions:", X.shape, Y.shape)
+    # Flatten (2803, 21, 3) -> (2803, 63)
+    data = X.reshape(X.shape[0], -1)
+    print("New shape:", data.shape)
+    X_train, X_test, Y_train, Y_test = train_test_split(data, Y, test_size=0.2, random_state=42)
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, Y_train)
+    print("Accuracy:", model.score(X_test, Y_test))
+    joblib.dump(model, "hand_gesture_model.pkl")
+
+def main():
+    file_path = "asl_alphabet_train/asl_alphabet_train" # Path to the dataset
+    data, labels = build_dataset(file_path)
+    train(data, labels)
+
+if __name__ == "__main__":    
+    main()
